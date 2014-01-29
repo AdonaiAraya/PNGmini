d948ff9aa594800a907b2e791bead79e420c2ede
AdonaiAraya <adonai@PC011.arteriastudios.com>
2014-01-29 15:26:58 +0100

    Puesta a produccion

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from cx_Freeze import setup, Executable
 
-includefiles = ["keys.json"]
+includefiles = ["keys.json", "PNGmini_logo16x16.ico", "PNGmini_logo32x32.ico", "PNGmini_logo64x64.ico", "PNGmini_logo128x128.ico", "PNGmini_logo_ayuda.gif", "README.md"]
 
 setup(
     name = "PNGmini",
-    version = "0.1",
-    description = "test",
+    version = "0.5",
+    description = "Aplicación de escritorio realizada en python para comprimir imágenes PNG sin perdida de calidad",
     author = "Adonai Araya",
     author_email = "adoargu@gmail.com",
     url = "http://www.doblea.nom.es",
@@ -18,7 +18,8 @@ setup(
             "main.py",
             base = "Win32GUI",
             shortcutName = "PNGmini",
-            shortcutDir = "DesktopFolder"
+            shortcutDir = "DesktopFolder",
+            icon = "PNGmini_logo128x128.ico"
         )
     ]
 )
